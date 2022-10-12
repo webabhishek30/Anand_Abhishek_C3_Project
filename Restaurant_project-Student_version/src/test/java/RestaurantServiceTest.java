a1fedf64810c4eb2c4a184ccff26f145e4afed74
web.abhishek30 <web.abhishek30@gmail.com>
2022-10-12 18:57:53 +0530

    Part 3: Failing test case

--- Restaurant_project-Student_version/src/test/java/RestaurantServiceTest.java
+++ Restaurant_project-Student_version/src/test/java/RestaurantServiceTest.java
@@ -24,13 +24,16 @@ class RestaurantServiceTest {
     @Test
     public void searching_for_existing_restaurant_should_return_expected_restaurant_object() throws restaurantNotFoundException {
         //WRITE UNIT TEST CASE HERE
-
+        assertEquals("Amelie's cafe", service.findRestaurantByName("Amelie's cafe").getName());
     }
 
     //You may watch the video by Muthukumaran on how to write exceptions in Course 3: Testing and Version control: Optional content
     @Test
     public void searching_for_non_existing_restaurant_should_throw_exception() throws restaurantNotFoundException {
         //WRITE UNIT TEST CASE HERE
+        assertThrows(restaurantNotFoundException.class,()->{
+            service.findRestaurantByName("Amelia");
+        });
 
     }
     //<<<<<<<<<<<<<<<<<<<<SEARCHING>>>>>>>>>>>>>>>>>>>>>>>>>>
